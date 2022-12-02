7bd0ea5b440bd6aa7e5e2c54ba24dd755ea390dd
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2022-12-01 23:51:41 -0600

    further update on category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,13 +37,33 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const createCategory = await Category.create(req.body);
+    res.status(200).json(createCategory);
+  } 
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+  try {
+    const updateCategory = await Category.update({
+      category_name: req.body.category_name,}
+      {
+        where: {
+          id: req.params.id,}
+        },
+      }
+    );
+
+  
+  catch (err) {
+    res.status(400).json(err);
+  }
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
